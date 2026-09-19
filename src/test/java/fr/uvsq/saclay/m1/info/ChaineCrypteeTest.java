f72e188b4ed9a0ab409e4096c1683bd1b17291b1
Abdoul Khadre Diop <adkhadre01@gmail.com>
2026-09-19 12:29:55 +0200

    Ajout du premier test de chiffrement

--- src/test/java/fr/uvsq/saclay/m1/info/ChaineCrypteeTest.java
+++ src/test/java/fr/uvsq/saclay/m1/info/ChaineCrypteeTest.java
@@ -1,4 +1,15 @@
 package fr.uvsq.saclay.m1.info;
 
-public class ChaineCrypteeTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ChaineCrypteeTest {
+
+    @Test
+    void crypteUneChaineAvecUneCleDeUn() {
+        ChaineCryptee chaine = ChaineCryptee.deEnClair("ABC", 1);
+
+        assertEquals("BCD", chaine.crypte());
+    }
 }
