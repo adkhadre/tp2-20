342136751c19bcdb9f3afbedd09ca71cf7bebee7
Abdoul Khadre Diop <adkhadre01@gmail.com>
2026-09-19 13:14:02 +0200

    Implémentation du chiffrement simple

--- src/main/java/fr/uvsq/saclay/m1/info/ChaineCryptee.java
+++ src/main/java/fr/uvsq/saclay/m1/info/ChaineCryptee.java
@@ -1,4 +1,32 @@
 package fr.uvsq.saclay.m1.info;
 
 public class ChaineCryptee {
+
+    private String chaine;
+    private int decalage;
+
+    private ChaineCryptee(String chaine, int decalage) {
+        this.chaine = chaine;
+        this.decalage = decalage;
+    }
+
+    public static ChaineCryptee deEnClair(String chaine, int decalage) {
+        return new ChaineCryptee(chaine, decalage);
+    }
+
+    public String crypte() {
+        String resultat = "";
+
+        for (int i = 0; i < chaine.length(); i++) {
+            char caractere = chaine.charAt(i);
+
+            if (caractere >= 'A' && caractere <= 'Z') {
+                caractere = (char) ((caractere - 'A' + decalage) % 26 + 'A');
+            }
+
+            resultat += caractere;
+        }
+
+        return resultat;
+    }
 }
